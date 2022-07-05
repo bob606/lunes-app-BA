203aad356a128d1d5c1c131122beea76be2e2b2a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-07-05 11:38:55 +0200

    LUN-377: Upgrade to react-native v0.69.1

--- android/app/src/main/java/com/lunesappui/MainApplication.java
+++ android/app/src/main/java/com/lunesappui/MainApplication.java
@@ -4,21 +4,22 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.modules.i18nmanager.I18nUtil;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
-import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
-import net.no_mad.tts.TextToSpeechPackage;
-import net.no_mad.tts.TextToSpeechPackage;
-import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
-import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+import com.lunesappui.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import com.facebook.react.bridge.JSIModulePackage;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
+import net.no_mad.tts.TextToSpeechPackage;
+import com.horcrux.svg.SvgPackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -49,14 +50,23 @@ public class MainApplication extends Application implements ReactApplication {
           }
       };
 
+    private final ReactNativeHost mNewArchitectureNativeHost =
+            new MainApplicationReactNativeHost(this);
+
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+      if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+          return mNewArchitectureNativeHost;
+      } else {
+          return mReactNativeHost;
+      }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
 
