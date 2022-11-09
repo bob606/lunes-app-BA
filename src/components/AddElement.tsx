79f5524b7e45e46ce57bef54eb276fe6878c8ad7
lunars97 <alekbarova.aizhana@gmail.com>
2022-11-09 10:27:12 +0100

    LUN-414: centered text in Heading component and changed button and explanation styling

--- src/components/AddElement.tsx
+++ src/components/AddElement.tsx
@@ -13,10 +13,13 @@ const PressableContainer = styled(PressableOpacity)`
 const AddCustomDisciplineText = styled(SubheadingPrimary)`
   text-transform: uppercase;
   padding-left: ${props => props.theme.spacings.xs};
+  margin-right: ${props => props.theme.spacings.xs};
 `
 
 const Explanation = styled(ContentSecondary)`
   padding: 0 ${props => props.theme.spacings.xxl} 0 11%;
+  font-family: ${props => props.theme.fonts.contentFontBold};
+  color: ${props => props.theme.colors.textSecondary};
 `
 
 const FlexRow = styled.View`
@@ -37,8 +40,8 @@ const AddElement = ({ onPress, label, explanation }: AddElementProps): ReactElem
     <>
       <PressableContainer onPress={onPress}>
         <FlexRow>
-          <AddCircleIcon width={theme.spacingsPlain.lg} height={theme.spacingsPlain.lg} />
           <AddCustomDisciplineText>{label}</AddCustomDisciplineText>
+          <AddCircleIcon width={theme.spacingsPlain.lg} height={theme.spacingsPlain.lg} />
         </FlexRow>
       </PressableContainer>
       {!!explanation && <Explanation>{explanation}</Explanation>}
